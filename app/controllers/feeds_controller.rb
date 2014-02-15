a3f7a0ad0641d74522ec2279ad4837877a2db433
k_taniguchi <k_taniguchi@haw.co.jp>
2014-02-15 11:38:48 +0900

    パラメータを受け取れるようにしました

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,8 +1,19 @@
 class FeedsController < ApplicationController
+  # URLのサンプル
+  # /feeds?json=%7B"q"%3A"hoge"%2C"feeds"%3A%5B"http%3A%2F%2Fhoge.com"%2C"http%3A%2F%2Ffuga.com"%5D%7D
+  #
+  # q - フィルターの条件
+  # feeds - RSSフィードのリスト
+  #
   def show
+    query = show_params['q']
+    feeds = show_params['feeds']
+
+    p query
+    p feeds
   end
 
   def show_params
-
+    @show_params ||= JSON.parse params[:json]
   end
 end
